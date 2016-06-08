e85c63efecb0ace6cc8a991f2a744759ac827ff6
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-06-08 17:34:55 -0400

    This code along was very enlightening

--- objc-classy-methods/FISAppDelegate.m
+++ objc-classy-methods/FISAppDelegate.m
@@ -18,6 +18,54 @@
      
      */
     
+    
+    //Example of instance methods
+    NSString *katherine = @"Katherine";
+    NSString *katherineHepburn = [katherine stringByAppendingString:@" Hepburn"];
+    NSLog(@"%@", katherineHepburn);
+    
+    //Example of init method
+    NSString *james = @"James";
+    NSString *jamesStewart = [[NSString alloc] initWithFormat:@"%@ %@", james, @"Stewart"];
+    NSLog(@"%@", jamesStewart);
+    
+    //Example of class method
+    NSString *queen = @"Queen";
+    NSString *queenElizabethII = [NSString stringWithFormat:@"%@ %@ %@", queen, @"Elizabeth", @"II"];
+    NSLog(@"%@", queenElizabethII);
+    
+    NSArray *classyThings = @[@"monocle", @"top hat", @"martini glass"];
+    NSLog(@"%@", classyThings);
+    
+    //Example of an initializer method
+    NSArray *classyPeople = [[NSArray alloc] initWithObjects:@"Katherine Hepburn", @"James Stewart", @"Queen Elizabeth II", nil];
+    NSLog(@"These people are the classiest: %@", classyPeople);
+    
+    
+    //Example of class method
+    NSArray *classyDrinks = [NSArray arrayWithObjects:@"Old Fashioned", @"Churchill Martini", @"Prosecco", nil];
+    NSLog(@"%@", classyDrinks);
+
+    NSDictionary *classyByLiteral = @{@"classy things" : @[@"monocle",
+                                                           @"top hat",
+                                                           @"martini glass"],
+                                      @"classy people" : @[@"Katherine Hepburn",
+                                                           @"James Stewart",
+                                                           @"Queen Elizabeth II"],
+                                      @"classy drinks" : @[@"Old Fashioned",
+                                                           @"Churchill Martini",
+                                                           @"Proseco"]};
+    NSLog(@"%@", classyByLiteral);
+    
+    //Example of using an initializer method
+    NSDictionary *classByInit = [[NSDictionary alloc] initWithObjectsAndKeys:classyThings, @"classy things", classyPeople, @"classy people", classyDrinks, @"classy drinks", nil];
+    NSLog (@"%@", classByInit);
+    
+    //Example of class method
+    NSDictionary *classByClass = [NSDictionary dictionaryWithObjects:@[classyThings, classyPeople, classyDrinks] forKeys:@[@"classy things", @"classy people", @"classy drinks"]];
+    NSLog(@"%@", classByClass);
+                                  
+    
     return YES;
 }
 
